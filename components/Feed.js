396d4f45888e7a356b2d34c5b16b90ca97187249
Calathea-Z <zsykes21@gmail.com>
2023-01-21 18:27:46 -0500

    Add dependencies for tailwinds scroll bars. Fix issue where feed was not sitting in the center of the view.

--- components/Feed.js
+++ components/Feed.js
@@ -2,8 +2,8 @@ import AllPosts from './AllPosts';
 
 function Feed() {
   return (
-    <main>
-        <section>
+    <main className='grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl mx-auto'>
+        <section className='col-span-2'>
             <AllPosts />
         </section>
 
