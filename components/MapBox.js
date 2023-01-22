05934dc552850bc12240a38278f08d56200a1907
Calathea-Z <zsykes21@gmail.com>
2023-01-22 12:04:52 -0500

    Added working map functionality. Connection to state and actual DB still need to be implemented. Also added in a log in page (no functionality).

--- components/MapBox.js
+++ components/MapBox.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import { useMemo } from 'react';
+import { useJsApiLoader, GoogleMap, Marker } from '@react-google-maps/api';
+import { RingLoader } from 'react-spinners';
 
-function MapBox() {
-  return (
-    <div>MapBox</div>
+const center = { lat: 48.8584, lng: 2.2945 }
+
+function MapBox() {  
+    const {isLoaded} = useJsApiLoader({
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+    })
+
+    if(!isLoaded) return <RingLoader color={'black'} size={50} aria-label="Loading"/>
+       
+return(
+    <div className='ml-4 mt-8 rounded-md border-2 border-gray-200'>
+        <GoogleMap center={center} zoom={12} 
+        mapContainerStyle={{ width: '100%', height: '800px'}}
+        >
+            <Marker position={center} />
+        </GoogleMap>
+    </div>
   )
 }
+
 
 export default MapBox
--- components/MiniProfile.js
+++ components/MiniProfile.js
@@ -6,11 +6,11 @@ function MiniProfile() {
   return (
     <div className='flex items-center justify-between mt-14 ml-10'>
         <Image src={Profile} alt='' className='rounded-full border p-[2px] w-16 h-16'/>
-        <div>
-            <h2>Zach</h2>
-            <h3>THanks for choosing Shoestring</h3>
+        <div className='flex-1 mx-4'>
+            <h2 className='font-bold'>Zach</h2>
+            <h3 className='text-sm text-gray-400'>Thanks for choosing Shoestring</h3>
         </div>
-        <button>Sign Out</button>
+        <button className='text-sky-300 text-sm'>Sign Out</button>
     </div>
   )
 }
