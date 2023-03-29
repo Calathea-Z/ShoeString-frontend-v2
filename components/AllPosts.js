9d9e4c819066b57cfec097c56837794a88e31635
coreyloftus <coreyloftus@gmail.com>
2023-03-29 17:24:54 -0400

    AllPosts.js, Add dbLocalHost string for testing via locally run express backend | Header.js, add Link to index route for ShoeString logo

--- components/AllPosts.js
+++ components/AllPosts.js
@@ -1,39 +1,38 @@
-import SinglePost from './SinglePost';
-import { useState, useEffect } from 'react';
-
-
+import SinglePost from "./SinglePost"
+import { useState, useEffect } from "react"
 
 const AllPosts = () => {
+    const dbHeroku = "https://shoe-string.herokuapp.com/posts"
+    const dbLocalHost = "http://localhost:4000/posts"
+    const [post, setPost] = useState({})
 
-  const [post, setPost] = useState({});
-
-  const getPosts = async () => {
-    try {
-      //Get data from BE
-      const response = await fetch('https://shoe-string.herokuapp.com/posts');
-      const allPosts = await response.json();
-      setPost(allPosts);
-      }catch (err){
-        console.log(err);
-      }
+    const getPosts = async () => {
+        try {
+            //Get data from BE
+            const response = await fetch(dbLocalHost)
+            const allPosts = await response.json()
+            setPost(allPosts)
+            console.log(allPosts)
+        } catch (err) {
+            console.log(err)
+        }
     }
 
-  useEffect(() => {
-      getPosts()
-  }, [post]);
+    useEffect(() => {
+        getPosts()
+    }, [])
 
-
-  return (
-<div className='flex flex-col-reverse'>
-  {post.allPosts?.map((post, index) => {
-    return(
-      <div>
-        <SinglePost key={index} username={post.username} _id={post._id} body={post.body} likes={post.likes} img={post.img} profile_img={post.profile_img} location={post.location}/>
-      </div>
-      )
-  })}
-</div>
-  )
-} 
+    return (
+        <div className="flex flex-col-reverse">
+            {post.allPosts?.map((post, index) => {
+                return (
+                    <div key={index}>
+                        <SinglePost key={index} username={post.username} _id={post._id} body={post.body} likes={post.likes} img={post.img} profile_img={post.profile_img} location={post.location} />
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
 
 export default AllPosts
--- components/Header.js
+++ components/Header.js
@@ -1,58 +1,60 @@
-import Image from 'next/image';
-import { BsSearch  }from 'react-icons/bs';
-import { AiFillHome } from 'react-icons/ai';
-import FullLogo from '../assets/full_logo.png';
-import BootsLogo from '../assets/boots_logo.png';
-import ProfilePic from '../assets/profile2.png';
-import { SlMenu } from 'react-icons/sl';
-import { MdOutlineEmail} from 'react-icons/md';
-import { SlPeople } from 'react-icons/sl';
-import { BsPlusCircle } from 'react-icons/bs';
-import { useRecoilState } from 'recoil';
-import { modalState } from '../atoms/modalAtom';
-
-
+import Image from "next/image"
+import { BsSearch } from "react-icons/bs"
+import { AiFillHome } from "react-icons/ai"
+import FullLogo from "../assets/full_logo.png"
+import BootsLogo from "../assets/boots_logo.png"
+import ProfilePic from "../assets/profile2.png"
+import { SlMenu } from "react-icons/sl"
+import { MdOutlineEmail } from "react-icons/md"
+import { SlPeople } from "react-icons/sl"
+import { BsPlusCircle } from "react-icons/bs"
+import { useRecoilState } from "recoil"
+import { modalState } from "../atoms/modalAtom"
+import Link from "next/link"
 
 function Header() {
-    const [open, setOpen] = useRecoilState(modalState);
+    const [open, setOpen] = useRecoilState(modalState)
 
     return (
-<div className='shadow-sm border-b bg-white sticky top-0 z-50'>
-{/* -----------Header Left */}
-                        {/* Logo for Small Screens */}
-    <div className='flex justify-between max-w-6xl mx-5 lg:mx-auto'>
-        <div className='relative hidden lg:inline-grid w-24 cursor-pointer'>
-            <Image src={ FullLogo } alt='Shoestring full logo' fill style={{objectFit: "contain"}} />
-        </div>
-                        {/* Logo for Larger Screens */}
-        <div className='relative w-10 lg:hidden flex-shrink-0 cursor-pointer'>
-            <Image src={ BootsLogo } alt='Shoestring icon logo' fill style={{objectFit: "contain"}} />
-        </div>
-
-{/* -----------HeaderMiddle */}
-        <div className='max-w-xs'>
-            <div className='relative mt-1 p-3 rounded-md'>
-                <div className='absolute inset-y-0 pl-3 flex items-center pointer-events-none'>
-                    <BsSearch className='h-5 w-5 text-gray-500' />
+        <div className="shadow-sm border-b bg-white sticky top-0 z-50">
+            {/* -----------Header Left */}
+            {/* Logo for Small Screens */}
+            <div className="flex justify-between max-w-6xl mx-5 lg:mx-auto">
+                <div className="relative hidden lg:inline-grid w-24 cursor-pointer">
+                    <Link href="/">
+                        <Image src={FullLogo} alt="Shoestring full logo" fill style={{ objectFit: "contain", width: "100%" }} />
+                    </Link>
+                </div>
+                {/* Logo for Larger Screens */}
+                <div className="relative w-10 lg:hidden flex-shrink-0 cursor-pointer">
+                    <Link href="/">
+                        <Image src={BootsLogo} alt="Shoestring icon logo" fill style={{ objectFit: "contain", width: "100%" }} />
+                    </Link>
                 </div>
 
-                <input className='bg-gray-50 block w-full pl-10 sm:text-sm border-gray-300 focus:ring-black focus:border-black rounded-md' type='text' placeholder='Search'/>
+                {/* -----------HeaderMiddle */}
+                <div className="max-w-xs">
+                    <div className="relative mt-1 p-3 rounded-md">
+                        <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
+                            <BsSearch className="h-5 w-5 text-gray-500" />
+                        </div>
+
+                        <input className="bg-gray-50 block w-full pl-10 sm:text-sm border-gray-300 focus:ring-black focus:border-black rounded-md" type="text" placeholder="Search" />
+                    </div>
+                </div>
+
+                {/* -----------HeaderRight*/}
+                <div className="flex items-center justify-end space-x-4">
+                    <AiFillHome className="navBtn" />
+                    <SlMenu className="h-6 md:hidden" />
+                    <BsPlusCircle className="navBtn" onClick={() => setOpen(true)} />
+                    <MdOutlineEmail className="navBtn" />
+                    <SlPeople className="navBtn" />
+                    <Image src={ProfilePic} alt="profile" className="h-10 w-10 rounded-full cursor-pointer" />
+                </div>
             </div>
         </div>
-
-{/* -----------HeaderRight*/}
-        <div className='flex items-center justify-end space-x-4'>
-        <AiFillHome className='navBtn' />
-        <SlMenu className='h-6 md:hidden' />
-        <BsPlusCircle className='navBtn' onClick={() => setOpen(true)} />
-        <MdOutlineEmail className='navBtn' />
-        <SlPeople className='navBtn' />
-        <Image src={ProfilePic} alt='profile' className='h-10 w-10 rounded-full cursor-pointer' />
-        </div>
-    </div>    
-</div>
-
-  )
+    )
 }
 
 export default Header
