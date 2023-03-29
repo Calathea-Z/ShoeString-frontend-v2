e03643d76eb70b364a467cfb42e37f266fb085fc
coreyloftus <coreyloftus@gmail.com>
2023-03-29 18:10:57 -0400

    firebase/config.js updated for Firebase v9 app starting syntax | apiKey from .env console logs but does not move through to firebase

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,33 @@
-import Head from 'next/head';
-import Header from '../components/Header';
-import Feed from '../components/Feed';
-import Modal from '../components/Modal';
+import Head from "next/head"
+import Header from "../components/Header"
+import Feed from "../components/Feed"
+import Modal from "../components/Modal"
+import { app } from "../firebase/config"
+import { getAuth } from "firebase/auth"
+import { useState, useEffect } from "react"
 
 export default function Home() {
-  return (
-    <div className='bg-gray-50 h-screen overflow-y-scroll scrollbar-hide'>
-      <Head>
-        <title>ShoeString</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Header />
-      <Feed />
-      <Modal />
-    </div>
-  )
+    const [isLoggedIn, setIsLoggedIn] = useState(false)
+    console.log(process.env.FIREBASE_API_KEY)
+    useEffect(() => {
+        const auth = getAuth(app)
+
+        const login = auth.onAuthStateChanged((user) => {
+            return user ? setIsLoggedIn(true) : setIsLoggedIn(false)
+        })
+        console.log({ "logged in?": isLoggedIn, user: auth.currentUser })
+        return login
+    }, [])
+
+    return (
+        <div className="bg-gray-50 h-screen overflow-y-scroll scrollbar-hide">
+            <Head>
+                <title>ShoeString</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <Header />
+            <Feed />
+            <Modal />
+        </div>
+    )
 }
